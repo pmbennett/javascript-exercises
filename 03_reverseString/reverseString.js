3d392cb674c0e801fe36a23430318a6211ad6833
Paul Bennett <paulbennett001@gmail.com>
2022-03-17 21:58:37 -0700

    exercise 3 complete, 4/4 test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let charArr = []
+    let len = string.length;
+    for (i = 0; i < len; i++) {
+        charArr[i] = string.substr(-(i + 1), 1);
+    }
+
+    let result = charArr.join("");
+    return result;
 };
 
 // Do not edit below this line
