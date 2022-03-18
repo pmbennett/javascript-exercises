ce39ac4c4192ca03e20daef458716794a68bd077
Paul Bennett <paulbennett001@gmail.com>
2022-03-17 21:03:59 -0700

    exercise 2 complete, 7/7 tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = "";
+    if (num < 0) {
+        result = "ERROR";
+    }
 
+    for (let i = 0; i < num; i++) {
+        if (i == 0) {
+            result = string;
+        } else {
+            result += string;
+        }
+    }
+    console.log(result);
+    return result;
 };
 
 // Do not edit below this line
