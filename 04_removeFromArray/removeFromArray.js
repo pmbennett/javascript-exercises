44a1f67ee4ffa91b982afba9246355edcd05d6fe
pmbennett <paulbennett001@gmail.com>
2022-03-20 19:36:01 -0700

    exercise 4 complete, 7/7 tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(char, ...args) {
+    let arr = [];
+    arr = char;
+    let remArr = [];
+    remArr = args;
 
+    for (let i = 0; i < remArr.length; i++) {
+        for (let j = 0; j < char.length; j++) {
+            if (char[j] === remArr[i]) {
+                char.splice(j, 1);
+            }
+        }
+    }
+
+    return char;
 };
 
 // Do not edit below this line
