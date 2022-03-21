34a95a95cfebb68a587774572eb555638aff3e31
pmbennett <paulbennett001@gmail.com>
2022-03-20 20:18:17 -0700

    exercise 5 complete, 6/6 tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let max = 0;
+    let min = 0;
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    } else if (num1 > num2) {
+        max = num1;
+        min = num2;
+    } else if (num1 < num2) {
+        max = num2;
+        min = num1;
+    }
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
